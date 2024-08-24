b343fc431513d483af900b9cc505ec7384b5a94c
eyi.lase <eyiowuawiidris44@gmail.com>
2024-08-23 18:33:09 -0700

    updated

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print all hexadecimal chars
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58 || i < 103)
+	{
+		if (i == 58)
+			i = 97;
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
