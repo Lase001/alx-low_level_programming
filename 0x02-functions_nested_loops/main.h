152ece2edb4bbbf9747342c5f861bab4043131b4
eyi.lase <eyiowuawiidris44@gmail.com>
2024-08-23 19:07:48 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+/* function prototypes for project tasks */
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int);
+
+/* function prototypes for advanced tasks */
+void print_times_table(int);
