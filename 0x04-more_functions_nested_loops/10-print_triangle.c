e7436cb11a48ea504520ab395670e8032a799026
eyi.lase <eyiowuawiidris44@gmail.com>
2024-08-23 23:19:48 -0700

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle on the terminal
+ * followed by a new line
+ * @n: number of lines
+ */
+void print_triangle(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 1; j < n - i; j++)
+			_putchar(' ');
+		for (j--; j < n; j++)
+			_putchar('#');
+		if (i < n - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - find product of two numbers
+ * @a: first interger argument
+ * @b: second integer argument
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
