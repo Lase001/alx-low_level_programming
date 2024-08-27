c5c5ba8ab734934578da3905a719aa49a69fddda
eyi.lase <eyiowuawiidris44@gmail.com>
2024-08-27 03:21:36 -0700

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: input string
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+		i = 1 + _strlen_recursion(s + 1);
+	return (i);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * prime_h - check if a number is a prime number
+ * @n: given number
+ * @i: iterator
+ * Return: 1 if n is a prime number.. 0 otherwise
+ */
+int prime_h(int n, int i)
+{
+	if (n % i == 0)
+	{
+		if (n == i)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + prime_h(n, i + 1));
+}
+
+/**
+ * is_prime_number - check if a number is a prime number
+ * @n: given number
+ * Return: 1 if n is a prime number.. 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+		return (0);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (prime_h(n, 2));
+}
