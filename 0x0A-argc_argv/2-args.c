b61243ccb483e5431b28c85043cd6e929bedf32a
eyi.lase <eyiowuawiidris44@gmail.com>
2024-08-27 19:30:30 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int p;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	p = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", p);
+	return (0);
+}
