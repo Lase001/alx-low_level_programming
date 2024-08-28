4449351e47616f71583f437cf115c410d7a2d546
Idris <eyiowuawiidris27@gmail.com>
2024-08-28 16:58:01 -0700

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the nodes of a list_t list
+ * @h: singly linked list
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		if (!h->str)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * list_len - return the number of nodes in a list
+ * @h: singly linked list
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
